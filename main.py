190e69fa2847f5786227bed68a498baefceeca1f
EnlightenedCSF <adamant94@mail.ru>
2015-02-22 14:38:06 +0400

    degenerative edges removed

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ __author__ = 'enlightenedcsf'
 import math
 import numpy as np
 import cv2
+import logging
 
 
 def nothing(x):
@@ -52,7 +53,7 @@ def find_unique_lines(input_lines):
     if (input_lines is None) or (len(input_lines) == 0):
         return None
     # if len(input_lines[0]) == 1:
-    #     return input_lines
+    # return input_lines
     new_lines = [(input_lines[0][0])]
 
     for r_in, t_in in input_lines[0]:
@@ -71,7 +72,7 @@ def find_unique_lines(input_lines):
             new_abc_lines.append(abc)
     # # new_abc_lines.append(get_a_b_c(r, t))
     # for r, t in new_lines:
-    #     new_abc_lines.append(get_a_b_c(r, t))
+    # new_abc_lines.append(get_a_b_c(r, t))
     return new_abc_lines
 
 
@@ -216,23 +217,23 @@ def is_point_dark(point, line):
         while cur < pi_2:
             cos = math.cos(cur)
             sin = math.sin(cur)
-            density.append(thres[point[1]+sin*1, point[0]+cos*1])
-            density.append(thres[point[1]+sin*3, point[0]+cos*3])
-            density.append(thres[point[1]+sin*6, point[0]+cos*6])
-            density.append(thres[point[1]+sin*9, point[0]+cos*9])
-            density.append(thres[point[1]+sin*12, point[0]+cos*12])
+            density.append(thres[point[1] + sin * 1, point[0] + cos * 1])
+            density.append(thres[point[1] + sin * 3, point[0] + cos * 3])
+            density.append(thres[point[1] + sin * 6, point[0] + cos * 6])
+            density.append(thres[point[1] + sin * 9, point[0] + cos * 9])
+            density.append(thres[point[1] + sin * 12, point[0] + cos * 12])
             cur += pi_8
 
-        # abc_vert = line[1] == 0
-        # tga_abc = np.inf
-        # if not abc_vert:
-        #     dx_abc = 0.1
-        #     dy_abc = (-line[0] * 0.1) / line[1]
-        #     tga_abc = dy_abc / dx_abc
-        #     cos = math.sqrt(1/(1+tga_abc*tga_abc))
-        #     sin = math.sqrt(1 - cos*cos)
-        #     if dy_abc < 0:
-        #         sin *= -1
+            # abc_vert = line[1] == 0
+            # tga_abc = np.inf
+            # if not abc_vert:
+            # dx_abc = 0.1
+            # dy_abc = (-line[0] * 0.1) / line[1]
+            # tga_abc = dy_abc / dx_abc
+            # cos = math.sqrt(1/(1+tga_abc*tga_abc))
+            # sin = math.sqrt(1 - cos*cos)
+            # if dy_abc < 0:
+            # sin *= -1
 
             # cv2.circle(img, (int(point[0]+cos*1),  int(point[1]+sin*1)), 1, (255, 0, 0), 2)
             # cv2.circle(img, (int(point[0]+cos*9),  int(point[1]+sin*9)), 1, (255, 0, 0), 2)
@@ -246,23 +247,23 @@ def is_point_dark(point, line):
             # cv2.circle(img, (int(point[0]-cos*6), int(point[1]+sin*9)), 1, (255, 0, 0), 2)
             # cv2.circle(img, (int(point[0]-cos*9), int(point[1]+sin*15)), 1, (255, 0, 0), 2)
 
-        #     density.append(thres[point[1]+sin*1, point[0]+cos*1])
-        #     density.append(thres[point[1]+sin*6, point[0]+cos*6])
-        #     density.append(thres[point[1]+sin*9, point[0]+cos*9])
-        #     density.append(thres[point[1]+sin*12, point[0]+cos*12])
+        # density.append(thres[point[1]+sin*1, point[0]+cos*1])
+        # density.append(thres[point[1]+sin*6, point[0]+cos*6])
+        # density.append(thres[point[1]+sin*9, point[0]+cos*9])
+        # density.append(thres[point[1]+sin*12, point[0]+cos*12])
         #
-        #     density.append(thres[point[1]-sin*1, point[0]-cos*1])
-        #     density.append(thres[point[1]-sin*6, point[0]-cos*6])
-        #     density.append(thres[point[1]-sin*9, point[0]-cos*9])
-        #     density.append(thres[point[1]-sin*12, point[0]-cos*12])
+        # density.append(thres[point[1]-sin*1, point[0]-cos*1])
+        # density.append(thres[point[1]-sin*6, point[0]-cos*6])
+        # density.append(thres[point[1]-sin*9, point[0]-cos*9])
+        # density.append(thres[point[1]-sin*12, point[0]-cos*12])
         #
-        #     density.append(thres[point[1]+sin*1, point[0]-cos*1])
-        #     density.append(thres[point[1]+sin*6, point[0]-cos*6])
-        #     density.append(thres[point[1]+sin*9, point[0]-cos*9])
-        #     density.append(thres[point[1]+sin*12, point[0]-cos*12])
+        # density.append(thres[point[1]+sin*1, point[0]-cos*1])
+        # density.append(thres[point[1]+sin*6, point[0]-cos*6])
+        # density.append(thres[point[1]+sin*9, point[0]-cos*9])
+        # density.append(thres[point[1]+sin*12, point[0]-cos*12])
         #
-        #     density.append(thres[point[1]-sin*1, point[0]+cos*1])
-        #     density.append(thres[point[1]-sin*6, point[0]+cos*6])
+        # density.append(thres[point[1]-sin*1, point[0]+cos*1])
+        # density.append(thres[point[1]-sin*6, point[0]+cos*6])
         #     density.append(thres[point[1]-sin*9, point[0]+cos*9])
         #     density.append(thres[point[1]-sin*12, point[0]+cos*12])
         # else:
@@ -282,16 +283,16 @@ def is_point_dark(point, line):
         for d in density:
             if d < 255:
                 cnt += 1
-        if cnt > 0:
-            cv2.circle(img, (int(point[0]),  int(point[1])), 1, (255, 0, 0), 2)
+        # if cnt > 0:
+        #     cv2.circle(img, (int(point[0]),  int(point[1])), 1, (255, 0, 0), 2)
         return cnt >= 1
 
     else:
         return False
 
 
-def are_points_close(p1, p2):
-    return math.sqrt((p2[0] - p1[0]) * (p2[0] - p1[0]) + (p2[1] - p1[1]) * (p2[1] - p1[1])) < 0.1
+def are_points_close(p1, p2, thres):
+    return math.sqrt((p2[0] - p1[0]) * (p2[0] - p1[0]) + (p2[1] - p1[1]) * (p2[1] - p1[1])) < thres
 
 
 def extract_vertices_and_edges(input_points, input_lines):
@@ -300,7 +301,7 @@ def extract_vertices_and_edges(input_points, input_lines):
     for line in input_lines:
         cnt += 20
 
-        start_new_edge = True
+        # start_new_edge = True
         points_on_line = get_points_on_line(line, input_points)
         points_on_edge = []
         for p in points_on_line:
@@ -310,19 +311,55 @@ def extract_vertices_and_edges(input_points, input_lines):
             continue
         for i in range(0, len(points_on_edge) - 1):
             if are_points_connected(points_on_edge[i], points_on_edge[i + 1], line):
-                if are_points_close(points_on_edge[i], points_on_edge[i + 1]):
+                if are_points_close(points_on_edge[i], points_on_edge[i + 1], 0.1):
                     continue
                 edges.append([points_on_edge[i], points_on_edge[i + 1]])
+                i += 1
                 # if start_new_edge:
-                #     edges.append([points_on_edge[i], points_on_edge[i + 1]])
-                #     start_new_edge = False
+                # edges.append([points_on_edge[i], points_on_edge[i + 1]])
+                # start_new_edge = False
                 # # if last point on a current edge is equal to a start one on the interval
                 # elif edges[len(edges) - 1][1] == points_on_edge[i]:
-                #     edges[len(edges) - 1][1] = points_on_edge[i + 1]
+                # edges[len(edges) - 1][1] = points_on_edge[i + 1]
                 # else:
-                #     edges.append([points_on_edge[i], points_on_edge[i + 1]])
+                # edges.append([points_on_edge[i], points_on_edge[i + 1]])
 
     return edges
+
+
+def delete_degenerate_edges(test_edges):
+    result_edges = []
+    for edge in test_edges:
+        if not are_points_close(edge[0], edge[1], 7):
+            result_edges.append(edge)
+
+    for edge1 in result_edges:
+        for edge2 in result_edges:
+            if edge1 is edge2:
+                continue
+            if are_points_close(edge1[0], edge2[0], 7) and are_points_close(edge1[1], edge2[1], 7) or are_points_close(
+                    edge1[0], edge2[1], 7) and are_points_close(edge1[1], edge2[0], 7):
+                result_edges.remove(edge2)
+                # to_delete = []
+                # for i in range(0, len(result_edges)):
+                # for j in range(0, len(result_edges)):
+                # if result_edges[i] is result_edges[j]:
+                # continue
+            # if are_points_close(result_edges[i][0], result_edges[j][0], 7) and are_points_close(result_edges[i][1],
+                # result_edges[j][1],
+                # 7) or are_points_close(
+                # result_edges[i][0], result_edges[j][1], 7) and are_points_close(result_edges[i][1],
+                # result_edges[j][0], 7):
+                # result_edges.remove(result_edges[j])
+                # if i > j:
+                # to_delete[j, i] = True
+                # else:
+                # to_delete[i, j] = True
+
+    # for x in to_delete.keys():
+    # result_edges. result_edges[x[1]])
+
+    return result_edges
 
 
 ########################################################################################################################
@@ -337,23 +374,23 @@ cv2.namedWindow('gray')
 img = cv2.imread("/home/enlightenedcsf/opencv/pyTest2/test2.jpg")
 # print(img.shape)
 # if img.shape[0] > img.shape[1]:
-#     img = cv2.resize(img, (1000 * img.shape[1] / img.shape[0], 1000))
+# img = cv2.resize(img, (1000 * img.shape[1] / img.shape[0], 1000))
 # else:
-#     img = cv2.resize(img, (1000, 1000 * img.shape[0] / img.shape[1]))
+# img = cv2.resize(img, (1000, 1000 * img.shape[0] / img.shape[1]))
 
 # imgC = img
 # img.convertTo(imgC, -1, 0.3, 0)
 
 # for i in range(1, img.shape[0]):
-#     for j in range(1, img.shape[1]):
-#         temp = img[i, j]
-#         temp *= 0.8
-#         # temp = int(temp)
-#         # for r, g, b in temp:
-#         #     r = int(1.3 * r)
-#         #     g = int(1.3 * g)
-#         #     b = int(1.3 * b)
-#         img[i, j] = temp
+# for j in range(1, img.shape[1]):
+# temp = img[i, j]
+# temp *= 0.8
+# # temp = int(temp)
+# # for r, g, b in temp:
+# #     r = int(1.3 * r)
+# #     g = int(1.3 * g)
+# #     b = int(1.3 * b)
+# img[i, j] = temp
 
 # print img.shape
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -376,9 +413,9 @@ while 1:
     # lines = cv2.HoughLines(edges, 3, np.pi / abs(90), 135)
     # if not ((lines is None) or (len(lines) == 0)):
     # for r, t in lines[0]:
-    #         a = np.cos(t)
-    #         b = np.sin(t)
-    #         x0 = a * r
+    # a = np.cos(t)
+    # b = np.sin(t)
+    # x0 = a * r
     #         y0 = b * r
     #         x1 = int(x0 + 1000 * (-b))
     #         y1 = int(y0 + 1000 * a)
@@ -405,13 +442,13 @@ while 1:
     for a, b, c in unique_lines:
         if b == 0:
             x = -c / a
-            cv2.line(img, (int(x), 0), (int(x), 1000), (0, 0, 255), 1)
+            # cv2.line(img, (int(x), 0), (int(x), 1000), (0, 0, 255), 1)
         else:
             x1 = 0
             y1 = -c / b
             x2 = 1000
             y2 = (-a * x2 - c) / b
-            cv2.line(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 1)
+            # cv2.line(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 1)
         cnt += 20
 
     # if not ((unique_lines is None) or (len(unique_lines) == 0)):
@@ -435,28 +472,37 @@ while 1:
 
     inter_points = find_intersections_in_abcs(unique_lines)
     # print(inter_points)
-    for x, y in inter_points:
-        cv2.circle(img, (int(x), int(y)), 3, (0, 255, 0), thickness=1)
+
+    # for x, y in inter_points:
+    #   cv2.circle(img, (int(x), int(y)), 3, (0, 255, 0), thickness=1)
 
     # unique_abc_lines = transform_lines(unique_lines)
     # test_lines = unique_abc_lines
 
     test_edges = extract_vertices_and_edges(inter_points, unique_lines)
+    # b = 15
+    # test_edges = test_edges[b: b+1]
+    # cv2.circle(img, (291, 299), 2, (0, 255, 0), thickness=1)
+    #     test_edges = [test_edges[9], test_edges[11]]
+    # test_edges = [test_edges[9]]
+    # test_edges = [test_edges[11]]
+    test_edges = delete_degenerate_edges(test_edges)
+    print(test_edges)
     cnt = 0
     for p1, p2 in test_edges:
-        cv2.line(img, (int(p1[0]), int(p1[1])), (int(p2[0]), int(p2[1])), (255, cnt * 2, cnt), thickness=3)
+        cv2.line(img, (int(p1[0]), int(p1[1])), (int(p2[0]), int(p2[1])), (255, cnt * 2, cnt), thickness=2)
         cnt += 5
 
-    # cnt = 0
-    # for a, b, c in test_lines:
-    #     if b == 0:
-    #         x = -c / a
-    #         cv2.line(img, (int(x), 0), (int(x), 1000), (0, 0, 255), 1)
-    #     else:
-    #         x1 = 0
-    #         y1 = -c / b
-    #         x2 = 1000
-    #         y2 = (-a * x2 - c) / b
-    #         cv2.line(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 1)
-    #     cnt += 20
+        # cnt = 0
+        # for a, b, c in test_lines:
+        #     if b == 0:
+        #         x = -c / a
+        #         cv2.line(img, (int(x), 0), (int(x), 1000), (0, 0, 255), 1)
+        #     else:
+        #         x1 = 0
+        #         y1 = -c / b
+        #         x2 = 1000
+        #         y2 = (-a * x2 - c) / b
+        #         cv2.line(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 1)
+        #     cnt += 20
 
